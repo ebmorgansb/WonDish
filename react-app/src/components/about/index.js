c26c644441a9b3cc21864afdd1c961b310aa5cbc
evan <ebmorgansb@gmail.com>
2023-04-17 12:46:12 -0700

    Finishing touches on about us page

--- react-app/src/components/about/index.js
+++ react-app/src/components/about/index.js
@@ -3,6 +3,10 @@ import {useEffect, useState} from 'react'
 import { useHistory } from 'react-router-dom'
 import './index.css'
 import about1 from '../../allImages/about1.jpg'
+import about2 from '../../allImages/about2.jpg'
+import about3 from '../../allImages/about3.jpg'
+import about4 from '../../allImages/about4.jpg'
+
 export default function About() {
   const history = useHistory()
   const dispatch = useDispatch()
@@ -20,20 +24,31 @@ export default function About() {
 
 return (
 <div className='aboutTotal'>
-  <div className='aboutTitle'>
+{/* <div class="aboutContainer">Some Text</div> */}
+  {/* <img className='aboutTitleImg' src={about4}> */}
+  {/* <div className='aboutTitle'>
   Why WonDish?
-  </div>
+  </div> */}
+  <div class="parentContainer">
+  <img className='aboutTitleImg' src={about4} alt="Landscape"></img>
+    <div class="centered">Why WonDish?</div>
+    </div>
   <div className='aboutText'>
     <div className='aboutImgTxt'>
-  <p>Welcome to WonDish, a platform that helps you find and review the best dishes in town. We understand that when it comes to food, everyone has their own preferences and cravings. That's why we've created a platform that focuses on individual dishes.</p>
+  <p className='aboutP'>Welcome to WonDish, a platform that helps you find and review the best dishes in town. We understand that when it comes to food, everyone has their own preferences and cravings. That's why we've created a platform that focuses on individual dishes.</p>
       <br/>
     <img className='aboutImg' src={about1} ></img>
     </div>
-      <p>At WonDish, we believe that finding the best dishes should be easy and accessible. Whether you're in the mood for pizza, sushi, or enchiladas, we've got you covered. Our platform allows you to search for a specific dish and see reviews and ratings from other users who have tried it.</p>
+    <div className='aboutImgTxt'>
+      <img className='aboutImg' src={about2} ></img>
+      <p className='aboutP'>At WonDish, we believe that finding the best dishes should be easy and accessible. Whether you're in the mood for pizza, sushi, or enchiladas, we've got you covered. Our platform allows you to search for a specific dish and see reviews and ratings from other users who have tried it.</p>
       <br/>
-      <p>We're passionate about food and dedicated to providing our users with the best possible experience. We believe that peer reviews are the most valuable source of information when it comes to food, and that's why we've created a platform that puts the power in your hands. As a user of WonDish, you have the ability to add your own reviews and contribute to the platform, making it even more valuable for others who are looking for the best dishes in town.</p>
+    </div>
+    <div className='aboutImgTxt'>
+      <p className='aboutP'>We believe that peer reviews are the most valuable source of information when it comes to food, and that's why we've created a platform that puts the power in your hands. As a user of WonDish, you have the ability to add your own reviews and contribute to the platform, making it even more valuable for others who are looking for the best dishes in town. Join our community today and start discovering the best dishes in town!</p>
+      <img className='aboutImg' src={about3} ></img>
       <br/>
-      <p>Whether you're a foodie looking to discover new dishes or a casual diner trying to find the best spot for your favorite meal, WonDish has something for everyone. Join our community today and start discovering the best dishes in town!</p>
+    </div>
   </div>
 </div>
 )
